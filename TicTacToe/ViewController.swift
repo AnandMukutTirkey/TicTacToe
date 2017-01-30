1f8de778aa5366eed46ca766525724309998089b
Anand Mukut Tirkey <anand@Anands-MacBook-Pro.local>
2017-01-30 15:42:42 +0530

    first commit with untested AI

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
             alert.addAction(dismissAction)
             self.present(alert, animated: true, completion: nil)
         }else{
-            if turn == 0 {
+            /*if turn == 0 {
                 sender.setImage(#imageLiteral(resourceName: "zero"), for: UIControlState.normal)
                 array[row][column] = 0
                 turn = 1
@@ -52,9 +52,13 @@ class ViewController: UIViewController {
                 sender.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
                 array[row][column] = 1
                 turn = 0
-            }
+            }*/
+            sender.setImage(#imageLiteral(resourceName: "zero"), for: UIControlState.normal)
+            array[row][column] = 0
             //#MARK: check if game has finished
             let result = isGameFinished(array: array)
+            let tuple = winMove(array: array, turn: 1)
+            let tuple2 = winMove(array: array, turn: 0)
             if result == true {
                 let winner : String?
                 if turn == 0{
@@ -79,19 +83,110 @@ class ViewController: UIViewController {
                 }
                 alert.addAction(yesAction)
                 self.present(alert, animated: true, completion: nil)
+                //#MARK: goto end of func
+            }else if tuple.0 != 3 {//#MARK: check for the possibility of computer winning
+                array[tuple.0][tuple.1] = 1
+                switch tuple {
+                case (0,0):
+                    btn0.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                case (0,1):
+                    btn1.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                case (0,2):
+                    btn2.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                case (1,0):
+                    btn3.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                case (1,1):
+                    btn4.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                case (1,2):
+                    btn5.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                case (2,0):
+                    btn6.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                case (2,1):
+                    btn7.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                case (2,2):
+                    btn8.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                default:
+                    print("error")
+                }
+                let alert = UIAlertController(title: "Cross Won", message: "do you want to restart", preferredStyle: UIAlertControllerStyle.alert)
+                let yesAction = UIAlertAction(title: "YES", style: UIAlertActionStyle.default) {
+                    (result : UIAlertAction) -> Void in
+                    self.array = [[3,3,3],[3,3,3],[3,3,3]]
+                    self.btn1.setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                    self.btn2.setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                    self.btn3.setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                    self.btn4.setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                    self.btn5.setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                    self.btn6.setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                    self.btn7.setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                    self.btn8.setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                    self.btn0.setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                    self.turn = 0
+                }
+                alert.addAction(yesAction)
+                self.present(alert, animated: true, completion: nil)
+                //#MARK: goto end of func
             }
-            //#MARK: check for the possibility of winning
-            
-            //#MARK: check for the possibility of users winning
-            
-            //#MARK: pick center cell
-            
-            //#MARK: pick corner cell
-            
+            else if tuple2.0 != 3 {//#MARK: check for the possibility of users winning
+                array[tuple2.0][tuple2.1] = 1
+                switch tuple2 {
+                    case (0,0):
+                        btn0.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                    case (0,1):
+                        btn1.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                    case (0,2):
+                        btn2.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                    case (1,0):
+                        btn3.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                    case (1,1):
+                        btn4.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                    case (1,2):
+                        btn5.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                    case (2,0):
+                        btn6.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                    case (2,1):
+                        btn7.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                    case (2,2):
+                        btn8.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                    default:
+                        print("error")
+                }
+            }else if array[1][1] == 3 {//#MARK: pick center cell
+                array[1][1] = 1
+                btn4.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+            }else if array[0][0] == 3 {//#MARK: pick corner cell
+                array[0][0] = 1
+                btn0.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+            }else if array[0][2] == 3{
+                array[0][2] = 1
+                btn2.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+            }else if array[2][0] == 3{
+                array[2][0] = 1
+                btn6.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+            }else if array[2][2] == 3{
+                array[2][2] = 1
+                btn8.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+            }else if array[0][1] == 3{
+                array[0][1] = 1
+                btn1.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+            }else if array[1][0] == 3{
+                array[1][0] = 1
+                btn3.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+            }else if array[1][2] == 3{
+                array[1][2] = 1
+                btn5.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+            }else if array[2][1] == 3{
+                array[2][1] = 1
+                btn7.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+            }else{
+                print("what cell untouched")
+            }
             //#MARK: pi
+            print(array)
         }
     }
 }
+
 
 func isGameFinished(array : [[Int]]) -> Bool {
     var turn = 1;
@@ -132,4 +227,56 @@ func isGameFinished(array : [[Int]]) -> Bool {
     }
     return false
 }
-
+func winMove(array : [[Int]] , turn : Int) -> (Int,Int) {
+    //let turn = 1;
+    if array[0][0] == turn && array[1][0] == turn{
+        return (2,0)
+    }else if array[1][0] == turn && array[2][0] == turn{
+        return (0,0)
+    }else if array[0][0] == turn && array[2][0] == turn{
+        return (1,0)
+    }else if array[0][1] == turn && array[1][1] == turn{
+        return (2,1)
+    }else if array[0][1] == turn && array[2][1] == turn{
+        return (1,1)
+    }else if array[1][1] == turn && array[2][1] == turn{
+        return (0,1)
+    }else if array[0][2] == turn && array[1][2] == turn{
+        return (2,2)
+    }else if array[0][2] == turn && array[2][2] == turn{
+        return (1,2)
+    }else if array[1][2] == turn && array[2][2] == turn{
+        return (0,2)
+    }else if array[0][0] == turn && array[0][1] == turn{
+        return (0,2)
+    }else if array[0][1] == turn && array[0][2] == turn{
+        return (0,0)
+    }else if array[0][2] == turn && array[0][0] == turn{
+        return (0,1)
+    }else if array[1][0] == turn && array[1][1] == turn{
+        return (1,2)
+    }else if array[1][1] == turn && array[1][2] == turn{
+        return (1,0)
+    }else if array[1][2] == turn && array[1][0] == turn{
+        return (1,1)
+    }else if array[2][0] == turn && array[2][1] == turn{
+        return (2,2)
+    }else if array[2][1] == turn && array[2][2] == turn{
+        return (2,0)
+    }else if array[2][2] == turn && array[2][0] == turn{
+        return (2,1)
+    }else if array[0][0] == turn && array[1][1] == turn{
+        return (2,2)
+    }else if array[1][1] == turn && array[2][2] == turn{
+        return (0,0)
+    }else if array[2][2] == turn && array[0][0] == turn{
+        return (1,1)
+    }else if array[0][2] == turn && array[1][1] == turn{
+        return (2,0)
+    }else if array[1][1] == turn && array[2][0] == turn{
+        return (0,2)
+    }else if array[2][0] == turn && array[0][2] == turn{
+        return (1,1)
+    }
+    return (3,3)
+}
