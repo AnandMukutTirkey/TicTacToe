4580b49dd6f1902e5b9bf2a56a4a5cdd9448c645
Anand Mukut Tirkey <anandtirkey96@gmail.com>
2020-01-18 17:31:37 +0530

    UI revamp

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -29,8 +29,8 @@ class ViewController: UIViewController {
         let column = id%3
         let row = id/3
         if array[row][column] != 3 {
-            let alert = UIAlertController(title: "Invalid Move", message: "You have made an invalid move please choose empty cell", preferredStyle: UIAlertControllerStyle.alert)
-            let dismissAction = UIAlertAction(title: "dismiss", style: UIAlertActionStyle.default) {
+            let alert = UIAlertController(title: "Invalid Move", message: "You have made an invalid move please choose empty cell", preferredStyle: UIAlertController.Style.alert)
+            let dismissAction = UIAlertAction(title: "dismiss", style: UIAlertAction.Style.default) {
                 (result : UIAlertAction) -> Void in
                 print("dismiss")
             }
@@ -38,11 +38,11 @@ class ViewController: UIViewController {
             self.present(alert, animated: true, completion: nil)
         }else{
             if turn == 0 {
-                sender.setImage(#imageLiteral(resourceName: "zero"), for: UIControlState.normal)
+                sender.setImage(#imageLiteral(resourceName: "zero"), for: UIControl.State.normal)
                 array[row][column] = 0
                 turn = 1
             }else if turn == 1{
-                sender.setImage(#imageLiteral(resourceName: "cross"), for: UIControlState.normal)
+                sender.setImage(#imageLiteral(resourceName: "cross"), for: UIControl.State.normal)
                 array[row][column] = 1
                 turn = 0
             }
@@ -55,12 +55,12 @@ class ViewController: UIViewController {
                 }else{
                     winner = "zero"
                 }
-                let alert = UIAlertController(title: winner! + "Won", message: "do you want to restart", preferredStyle: UIAlertControllerStyle.alert)
-                let yesAction = UIAlertAction(title: "YES", style: UIAlertActionStyle.default) {
+                let alert = UIAlertController(title: winner! + "Won", message: "do you want to restart", preferredStyle: UIAlertController.Style.alert)
+                let yesAction = UIAlertAction(title: "YES", style: UIAlertAction.Style.default) {
                     (result : UIAlertAction) -> Void in
                     self.array = [[3,3,3],[3,3,3],[3,3,3]]
                     for i in 0...self.buttons.count-1{
-                        self.buttons[i].setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                        self.buttons[i].setImage(#imageLiteral(resourceName: "blank"), for: UIControl.State.normal)
                     }
                     self.turn = 0
                 }
@@ -68,12 +68,12 @@ class ViewController: UIViewController {
                 self.present(alert, animated: true, completion: nil)
             }
             if turns == 9 && result == false{
-                let alert = UIAlertController(title: "Draw", message: "do you want to restart", preferredStyle: UIAlertControllerStyle.alert)
-                let yesAction = UIAlertAction(title: "YES", style: UIAlertActionStyle.default) {
+                let alert = UIAlertController(title: "Draw", message: "do you want to restart", preferredStyle: UIAlertController.Style.alert)
+                let yesAction = UIAlertAction(title: "YES", style: UIAlertAction.Style.default) {
                     (result : UIAlertAction) -> Void in
                     self.array = [[3,3,3],[3,3,3],[3,3,3]]
                     for i in 0...self.buttons.count-1{
-                        self.buttons[i].setImage(#imageLiteral(resourceName: "blank"), for: UIControlState.normal)
+                        self.buttons[i].setImage(#imageLiteral(resourceName: "blank"), for: UIControl.State.normal)
                     }
                     self.turn = 0
                     self.turns = 0
